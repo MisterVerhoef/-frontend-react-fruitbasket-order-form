a4cc94b35da92582c54c506f68b5fefd85499888
MisterVerhoef <frankverhoef@outlook.com>
2022-05-16 18:06:09 +0200

    het werkt maar geen styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import CountingFruit from "./Components/CountingFruit";
+import RadioButton from "./Components/radioButton";
 import strawberry from "./assets/strawberry.png";
 import banana from "./assets/banana.png";
 import apple from "./assets/apple.png";
@@ -8,6 +9,7 @@ import logo from "./assets/screenshot-logo.png";
 
 
 import './App.css';
+
 
 function App() {
     const [aardbeiCounter, setAardbeiCounter] = useState(0);
@@ -23,7 +25,16 @@ function App() {
     const [newsletterValue, setNewsletterValue] = React.useState(false);
 
     const [bezorgFrequentie, setBezorgFrequentie] = useState('');
-    const [dagdeelBezorging, setDagdeelBezorging] = useState('');
+
+    const [dagDeel, setDagdeel] = React.useState('Overdag');
+
+    const handleOverdagChange = () => {
+        setDagdeel('Overdag');
+    };
+
+    const handleAvondChange = () => {
+        setDagdeel('Avond');
+    };
 
     const handleChange = (event) => {
         setBezorgFrequentie(event.target.value);
@@ -41,136 +52,133 @@ function App() {
 
     function handleSubmit(e) {
         e.preventDefault();
-        let aardbeiCount =  aardbeiCounter + "x aardbeien";
-        let banaanCount =  banaanCounter + "x bananen";
-        let appelCount =  appelCounter + "x appels";
-        let kiwiCount =  kiwiCounter + "x kiwi's";
-        let termsChecked = "akkoord voor nieuwsbrief = " +newsletterValue;
+        let aardbeiCount = aardbeiCounter + "x aardbeien";
+        let banaanCount = banaanCounter + "x bananen";
+        let appelCount = appelCounter + "x appels";
+        let kiwiCount = kiwiCounter + "x kiwi's";
+        let termsChecked = "akkoord voor nieuwsbrief = " + newsletterValue;
 
-        console.log(nameValue, lastNameValue, ageValue, postcodeValue,dagdeelBezorging, bezorgFrequentie,commentsValue, termsChecked, aardbeiCount, banaanCount, appelCount, kiwiCount);
+        console.log(nameValue, lastNameValue, ageValue, postcodeValue, dagDeel, bezorgFrequentie, commentsValue, termsChecked, aardbeiCount, banaanCount, appelCount, kiwiCount);
 
     }
 
     return (
         <>
-            <h1>Fruitmand bezorgservice</h1>
-            <img src={logo} alt="logoImage"/>
-            <article id="fruit-counter">
-                <CountingFruit fruitImage={strawberry} fruitImageText={"aardbei"}  fruitName={"Aardbeien"} fruitCounter={aardbeiCounter}
-                               setFruitCounter={setAardbeiCounter}/>
-                <CountingFruit fruitImage={banana} fruitImageText={"banaan"} fruitName={"Bananen"} fruitCounter={banaanCounter} setFruitCounter={setBanaanCounter}/>
-                <CountingFruit fruitImage={apple} fruitImageText={"appel"} fruitName={"Appels"} fruitCounter={appelCounter} setFruitCounter={setAppelCounter}/>
-                <CountingFruit fruitImage={kiwi} fruitImageText={"kiwi"} fruitName={"Kiwi's"} fruitCounter={kiwiCounter} setFruitCounter={setKiwiCounter}/>
-                <button
-                    type="button"
-                    onClick={ResetCounter}>
-                    Reset
-                </button>
-            </article>
+            <header>
+                <img src={logo} alt="logoImage"/>
+            </header>
+            <main>
+                <article id="fruit-counter">
+                    <CountingFruit fruitImage={strawberry} fruitImageText={"aardbei"} fruitName={"Aardbeien"}
+                                   fruitCounter={aardbeiCounter}
+                                   setFruitCounter={setAardbeiCounter}/>
+                    <CountingFruit fruitImage={banana} fruitImageText={"banaan"} fruitName={"Bananen"}
+                                   fruitCounter={banaanCounter} setFruitCounter={setBanaanCounter}/>
+                    <CountingFruit fruitImage={apple} fruitImageText={"appel"} fruitName={"Appels"}
+                                   fruitCounter={appelCounter} setFruitCounter={setAppelCounter}/>
+                    <CountingFruit fruitImage={kiwi} fruitImageText={"kiwi"} fruitName={"Kiwi's"}
+                                   fruitCounter={kiwiCounter} setFruitCounter={setKiwiCounter}/>
+                    <button
+                        type="button"
+                        onClick={ResetCounter}>
+                        Reset
+                    </button>
+                </article>
 
 
-            <form id="form-layout" onSubmit={handleSubmit}>
+                <form id="form-layout" onSubmit={handleSubmit}>
 
-                <label htmlFor="Voornaam">Voornaam:
-                    <input type="text"
-                           id="Voornaam"
-                           name="Voornaam"
-                           value={nameValue}
-                           onChange={(e) => setNameValue(e.target.value)}
-                    />
-                </label>
+                    <label htmlFor="Voornaam">Voornaam:
+                        <input type="text"
+                               id="Voornaam"
+                               name="Voornaam"
+                               value={nameValue}
+                               onChange={(e) => setNameValue(e.target.value)}
+                        />
+                    </label>
 
-                <label htmlFor="Achternaam"> Achternaam:
-                    <input type="text"
-                           id="Achternaam"
-                           name="Achternaam"
-                           value={lastNameValue}
-                           onChange={(e) => setLastNameValue(e.target.value)}
-                    />
-                </label>
+                    <label htmlFor="Achternaam"> Achternaam:
+                        <input type="text"
+                               id="Achternaam"
+                               name="Achternaam"
+                               value={lastNameValue}
+                               onChange={(e) => setLastNameValue(e.target.value)}
+                        />
+                    </label>
 
-                <label htmlFor="Leeftijd">Leeftijd:
-                    <input type="text"
-                           id="Leeftijd"
-                           name="Leeftijd"
-                           value={ageValue}
-                           onChange={(e) => setAgeValue(e.target.value)}
-                    />
-                </label>
+                    <label htmlFor="Leeftijd">Leeftijd:
+                        <input type="text"
+                               id="Leeftijd"
+                               name="Leeftijd"
+                               value={ageValue}
+                               onChange={(e) => setAgeValue(e.target.value)}
+                        />
+                    </label>
 
-                <label htmlFor="Postcode"> Postcode:
-                    <input type="text"
-                           id="Postcode"
-                           name="Postcode"
-                           value={postcodeValue}
-                           onChange={(e) => setPostcodeValue(e.target.value)}
-                    />
-                </label>
+                    <label htmlFor="Postcode"> Postcode:
+                        <input type="text"
+                               id="Postcode"
+                               name="Postcode"
+                               value={postcodeValue}
+                               onChange={(e) => setPostcodeValue(e.target.value)}
+                        />
+                    </label>
 
-                <p>Bezorgfrequentie</p>
-                <label htmlFor="dropdown-menu"></label>
-                <select value={bezorgFrequentie} onChange={handleChange}>
-                    <option value="Iedere Week">Iedere Week</option>
-                    <option value="Om de week">Om de week</option>
-                    <option value="Iedere maand">Iedere maand</option>
-                </select>
+                    <p>Bezorgfrequentie</p>
+                    <label htmlFor="dropdown-menu"></label>
+                    <select value={bezorgFrequentie} onChange={handleChange}>
+                        <option value="Iedere Week">Iedere Week</option>
+                        <option value="Om de week">Om de week</option>
+                        <option value="Iedere maand">Iedere maand</option>
+                    </select>
 
-                <label htmlFor="dagdeel-bezorging-overdag">
-                    <input
-                        type="radio"
-                           name="dagdeel-bezorging"
-                           id="dagdeel-bezorging-overdag"
-                           value="overdag"
-                           checked={dagdeelBezorging === "Overdag"}
-                           onClick={(e) => setDagdeelBezorging("Overdag")}
-                           onChange={(e) => setDagdeelBezorging(e.target.value)}
+                    <label htmlFor="dagdeel-bezorging-overdag">
+                        <RadioButton
+                            label="Overdag"
+                            value={dagDeel === 'Overdag'}
+                            onChange={handleOverdagChange}
+                        />
+                    </label>
+                    <label htmlFor="dagdeel-bezorging-avond">
+                        <RadioButton
+                            label="Avond"
+                            value={dagDeel === 'Avond'}
+                            onChange={handleAvondChange}
+                        />
+                    </label>
+                    <label htmlFor="comments"> Opmerkingen:
+                        <br/>
+                        <textarea name="comments"
+                                  id="comments"
+                                  cols="50"
+                                  rows="5"
+                                  value={commentsValue}
+                                  onChange={(e) => setCommentsValue(e.target.value)}
+                        ></textarea>
+                    </label>
+                    <label htmlFor="voorwaarden">
+                        <input
+                            type="checkbox"
+                            id="voorwaarden"
+                            name="voorwaarden-akkoord"
+                            value={newsletterValue}
+                            onChange={(e) => setNewsletterValue(!newsletterValue)}
 
-                    />Overdag
-                </label>
-                <label htmlFor="dagdeel-bezorging-avond">
-                    <input
-                        type="radio"
-                        name="dagdeel-bezorging"
-                        id="dagdeel-bezorging-avond"
-                        value="avond"
-                        checked={dagdeelBezorging === "'s Avonds"}
-                        onClick={() => setDagdeelBezorging("'s Avonds")}
-                        onChange={(e) => setDagdeelBezorging(e.target.value)}
-
-                    />'s Avonds
-                </label>
-
-                <label htmlFor="comments"> Opmerkingen:
+                        />
+                        Ik ga akkoord met de voorwaarden
+                    </label>
                     <br/>
-                    <textarea name="comments"
-                              id="comments"
-                              cols="50"
-                              rows="5"
-                              value={commentsValue}
-                              onChange={(e) => setCommentsValue(e.target.value)}
-                    ></textarea>
-                </label>
-                <label htmlFor="voorwaarden">
-                    <input
-                        type="checkbox"
-                        id="voorwaarden"
-                        name="voorwaarden-akkoord"
-                        value={newsletterValue}
-                        onChange={(e) => setNewsletterValue(!newsletterValue)}
+                    <br/>
+                    <button
+                        type="submit"
+                        onClick={handleSubmit}
 
-                    />
-                    Ik ga akkoord met de voorwaarden
-                </label>
-                <br/>
-                <br/>
-                <button
-                    type="submit"
-                    onClick={handleSubmit}
+                    >
+                        Versturen
+                    </button>
+                </form>
+            </main>
 
-                >
-                    Versturen
-                </button>
-            </form>
 
         </>
     );
